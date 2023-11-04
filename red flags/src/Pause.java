173946787d891b29de65faccdf63eae747edcc63
Isabella Poon <111008022+IsabellaPoon@users.noreply.github.com>
2023-11-04 18:11:48 -0400

    Delete red flags directory

--- red flags/src/Pause.java
+++
@@ -1,9 +0,0 @@
-public class Pause {
-    public static void pauseTime(long milliseconds) {
-        try {
-            Thread.sleep(milliseconds);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-    }
-}
