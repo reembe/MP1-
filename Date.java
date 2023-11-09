e8452112d62717cad12c6940b4db81e568f73a89
Isabella Poon <111008022+IsabellaPoon@users.noreply.github.com>
2023-11-08 21:14:49 -0500

    Update Date.java

--- Date.java
+++ Date.java
@@ -273,7 +273,7 @@ public class Date {
             }
     }
 
-    public void dwC() {
+   public void dwC() {
         Pause pa = new Pause();
         Clear c = new Clear();
         c.clearScreen();
@@ -283,10 +283,10 @@ public class Date {
         String choice = date.nextLine();
         c.clearScreen();
         if (choice.equals("1")) {
-            System.out.println("Let's go to the grocery store then! I only have " + dateChris.getMoney() + " dollars though, so you can pay the rest of what I can't lol. \n\nDo you want Arizona?");
+            System.out.println("Let's go to the grocery store then! I only have " + dateChris.getMoney() + " dollars though, so you can pay the rest of what I can't lol. \n\nDo you want Arizona? (yes) (no)");
             dateChris.addRedFlag(1);
             String ari = date.nextLine();
-            if (ari.equals("yes")) {
+            if ((ari).equalsIgnoreCase("yes")) {
                 c.clearScreen();
                 System.out.print("Okay, I'll get 2 for me and 1 for you.");
                 dateChris.addGroceries(3);
@@ -297,14 +297,16 @@ public class Date {
             }
             pa.pauseTime(2000);
             c.clearScreen();
+            pa.pauseTime(2000);
             System.out.println("\nI want lunchables, which is only 10 dollars! What a steal! \n\nooooh I want some chocolates, but it's 50 dollars omg. \nYou wouldn't mind paying, right? \n\nLol, why am I even asking? Of course, you wouldn't mind.");
             dateChris.addRedFlag(3);
             dateChris.addGroceries(10 + 50);
-            pa.pauseTime(5000);
+            pa.pauseTime(3000);
             c.clearScreen();
             System.out.println("Do we need any more food?");
+            pa.pauseTime(3000);
             String needs = date.nextLine();
-            if (needs.equals("no")) {
+            if (needs.equalsIgnoreCase("no")) {
                 c.clearScreen();
                 System.out.println("Ok fire! Let's go pay now!, you got the bill right?");
             } else {
@@ -312,55 +314,67 @@ public class Date {
                 System.out.println("What other food do we need?");
                 date.nextLine();
                 c.clearScreen();
-                System.out.println("Are you really going to eat all that? \nYeah, we are not getting that. Let's go pay now.");
+                System.out.println("Are you really going to eat all that?");
+                pa.pauseTime(3000);
+                System.out.println("Yeah, we are not getting that. Let's go pay now.");
                 dateChris.addRedFlag(1);
             }
             double lol = dateChris.total();
             lol *= 1.18;
             pa.pauseTime(2000);
             c.clearScreen();
-            System.out.println("The total is " + lol + ". Thanks for paying for us babe! \nI had a feeling you were a generous person when I met you!");
+            System.out.println("The total is " + lol + ". Thanks for paying for us babe!");
+
             dateChris.addRedFlag(1);
-            System.out.println("\n*You and Rizz arrive at the park and sit down at your pre-prepared picnic setting. You guys eat for 50 minutes*\n");
+            System.out.println("\n*You and Rizz arrive at the park and sit down at your pre-prepared picnic setting. You guys eat for 50 minutes.*\n");
             System.out.println("Soooo..... I weigh 250 lb. How much do you weigh?");
             dateChris.addRedFlag(1);
             int weight = date.nextInt();
             if (weight > 100) {
                 c.clearScreen();
                 System.out.println("oh.");
-                System.out.println("*Gets up, packs his things and leaves* \n Bye fatty, I'll text you my gym location before I block you.");
+                pa.pauseTime(2000);
+                System.out.println("*Gets up, packs his things and leaves*");
+                pa.pauseTime(2000);
+                System.out.println("Bye fatty, I'll text you my gym location before I block you.");
                 dateChris.addRedFlag(1);
             } else {
                 c.clearScreen();
                 System.out.println("Ok cutie patoots! I'll call you later pook.");
                 dateChris.addRedFlag(2);
             }
-            pa.pauseTime(2000);
+            pa.pauseTime(3000);
 
 
         } else if (choice.equals("2")) {
-            System.out.println("Let's go stand on line for the biggest roller coaster!\nGee this line is pretty long. Im going to step away for a sec if you dont mind");
-            pa.pauseTime(2000);
+            System.out.println("Let's go stand on line for the biggest roller coaster!\nGee this line is pretty long. Im going to step away for a sec if you don't mind.");
+            pa.pauseTime(1000);
             System.out.println("\n*He comes back 30 minutes later eating a burger.*");
+            pa.pauseTime(1000);
             dateChris.addRedFlag(3);
-            System.out.println("\nThis is such a good burger oh my god.\n*He sees you staring at the burger*\n\nLOL girl stop staring ur burning holes in my food");
+            System.out.println("\nThis is such a good burger oh my god.");
+            pa.pauseTime(1000);
+            System.out.println("*He sees you staring at the burger*");
+            pa.pauseTime(1000);
+            System.out.println("LOL girl stop staring ur burning holes in my food");
             dateChris.addRedFlag(3);
-            pa.pauseTime(2000);
+            pa.pauseTime(1000);
             System.out.println("\n*you finally reach your turn and ride the roller coaster... but the whole time, Rizz is clutching onto your arm and screaming in your ear.*");
             dateChris.addRedFlag(2);
-            pa.pauseTime(2000);
+            pa.pauseTime(1000);
             System.out.println("Bruh that was the least scary roller coaster I've ever ridden. It was literally pathetic tbh.");
             dateChris.addRedFlag(2);
             System.out.println("\n*Decide if you want to call him out or not (yes or no)");
             String callOut = s.nextLine();
-            if (callOut.equals("yes")) {
+            if (callOut.equalsIgnoreCase("yes")) {
                 System.out.println("*You tell him that he was screaming the whole time*");
+                pa.pauseTime(1000);
                 System.out.println("Am I not allowed to yell because I'm a man? what kind of sexist b**** did I agree to go on a date with omg. Anyways. i just saw my high school sweet heart so I'm gonna go talk to her and actually have a good time.");
                 dateChris.addRedFlag(3);
-                pa.pauseTime(2000);
+                pa.pauseTime(1000);
             } else {
                 System.out.println("Go stand on line I'm going to go to the bathroom.");
-                pa.pauseTime(2000);
+                pa.pauseTime(1000);
                 dateChris.addRedFlag(2);
                 System.out.println("Heyy I'm back and look who I found along the way. " + user.getName() + " meet my high school sweet heart pookie Clarizza!");
                 dateChris.addRedFlag(2);
@@ -368,7 +382,7 @@ public class Date {
                 if (s.nextLine().equals("1")) {
                     System.out.println("*The date is now over*");
                 } else {
-                    System.out.println("Let's go get food\n*all three of you go up to a food cart*");
+                    System.out.println("Let's go get food.\n*all three of you go up to a food cart*");
                     dateChris.addRedFlag(1);
                     System.out.println("Soooo..... I weigh 250 lb. How much do you weigh?");
                     dateChris.addRedFlag(1);
@@ -376,7 +390,7 @@ public class Date {
                     if (weight > 100) {
                         c.clearScreen();
                         System.out.println("oh.");
-                        System.out.println("*Gets up, packs his things and leaves with Clarizza* \n Bye fatty, I'm gonna go have fun with my bae");
+                        System.out.println("*Gets up, packs his things and leaves with Clarizza* \nBye fatty, I'm gonna go have fun with my bae");
                         dateChris.addRedFlag(1);
                     } else {
                         c.clearScreen();
